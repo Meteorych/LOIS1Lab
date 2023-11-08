7b45c69dffb70ceca85bba60f355edb0c2721067
EugeneSamokhval <91789484+EugeneSamokhval@users.noreply.github.com>
2023-11-08 21:04:44 +0300

    modified:   main.py

--- main.py
+++ main.py
@@ -5,13 +5,14 @@
 Дата выполнения: 03.10.2023
 """
 
-#A={(a, 0.5), (b, 0.6)}
-#B={(c, 0.3), (d, 0.7)}
-#C={(e, 0.1), (f, 0.8)}
-#A~>B
+# A={(a, 0.5), (b, 0.6)}
+# B={(c, 0.3), (d, 0.7)}
+# C={(e, 0.1), (f, 0.8)}
+# A~>B
 
 import input_validator
 import prettytable
+
 
 def user_input_to_array(user_input: str):
     name_buffer = ""
@@ -36,13 +37,15 @@ def user_input_to_array(user_input: str):
                 temp_string = ''
             case _:
                 temp_string += sign
-    divided_elements_array = [pair.split(',') for pair in divided_elements_array]
+    divided_elements_array = [pair.split(',')
+                              for pair in divided_elements_array]
     return [name_buffer, [[pair[0], float(pair[1])] for pair in divided_elements_array]]
 
 
 def get_set():
     while True:
-        user_input_set_1 = input('Введите нечёткое множество в формате A={(a, 0.5), (b, 0.6)} ...\n')
+        user_input_set_1 = input(
+            'Введите нечёткое множество в формате A={(a, 0.5), (b, 0.6)} ...\n')
         if input_validator.input_check(user_input_set_1):
             processed_set_1 = user_input_to_array(user_input_set_1)
             break
@@ -52,14 +55,14 @@ def get_set():
     return processed_set_1
 
 # Implication rule
+
+
 def table_construction(starting_set_1, starting_set_2):
     result_table = [[] for pair in starting_set_1]
     for pair_x in range(len(starting_set_1)):
-        for pair_y in range(len(starting_set_2)) :
-            if starting_set_1[pair_x][1] < starting_set_2[pair_y][1]:
-                result_table[pair_x].append(1)
-            else:
-                result_table[pair_x].append(starting_set_2[pair_y][1])
+        for pair_y in range(len(starting_set_2)):
+            result_table[pair_x].append(
+                min(starting_set_2[pair_y][1]/starting_set_1[pair_x][1], 1))
     table_view = prettytable.PrettyTable()
     table_view.add_rows(result_table)
     print(table_view)
@@ -71,7 +74,8 @@ def table_solver(x_y_table):
     for set_x_sign in range(len(x_y_table[0])):
         processed_table.append([])
         for table_sign in range(len(x_y_table[2][0])):
-            processed_table[set_x_sign].append(x_y_table[0][set_x_sign][1] * x_y_table[2][set_x_sign][table_sign])
+            processed_table[set_x_sign].append(
+                x_y_table[0][set_x_sign][1] * x_y_table[2][set_x_sign][table_sign])
     table_view = prettytable.PrettyTable()
     table_view.add_rows(processed_table)
     print(table_view)
@@ -81,8 +85,10 @@ def table_solver(x_y_table):
         for row in range(len(processed_table)):
             temp_container.append(processed_table[row][column])
         list_of_max.append(max(temp_container))
-    resulting_set = [[x_y_table[1][max_possibility][0], list_of_max[max_possibility]] for max_possibility in range(len(list_of_max))]
+    resulting_set = [[x_y_table[1][max_possibility][0], list_of_max[max_possibility]]
+                     for max_possibility in range(len(list_of_max))]
     return resulting_set
+
 
 def divide_name_set(starting_sets):
     list_of_names = []
@@ -91,6 +97,7 @@ def divide_name_set(starting_sets):
         list_of_names.append(pair[0])
         list_of_sets[pair[0]] = pair[1]
     return list_of_sets, list_of_names
+
 
 def rule_to_machine(user_rule):
     result = ""
@@ -102,6 +109,7 @@ def rule_to_machine(user_rule):
         else:
             result += user_rule[sign]
     return result
+
 
 def postfix_writing(poland_writing, list_of_names):
     result = []
@@ -116,9 +124,8 @@ def postfix_writing(poland_writing, list_of_names):
                 result.append(stack.pop(0))
             stack.pop(0)
     while len(stack):
-         result.append(stack.pop(0))
+        result.append(stack.pop(0))
     return result
-
 
 
 def get_rule(list_of_names):
@@ -129,24 +136,28 @@ def get_rule(list_of_names):
         machine_version = rule_to_machine(rule)
         return postfix_writing(machine_version, list_of_names)
 
+
 def use_rule(postfix_rule, dict_of_sets, list_of_names):
     stack = []
     for sign in postfix_rule:
         if sign in list_of_names:
             stack.insert(0, dict_of_sets.get(sign))
         else:
-            solution = table_solver([stack[1], stack[0], table_construction(stack[1], stack[0])])
+            solution = table_solver(
+                [stack[1], stack[0], table_construction(stack[1], stack[0])])
             stack.pop(0)
             stack.pop(0)
             stack.insert(0, solution)
     return stack[0]
+
 
 def main():
     cycle = 'Да'
     while cycle == 'Да':
         number_of_sets = int(input("Введите количество посылок: "))
         try:
-            starting_sets, list_of_names = divide_name_set([get_set() for current_set in range(number_of_sets)])
+            starting_sets, list_of_names = divide_name_set(
+                [get_set() for current_set in range(number_of_sets)])
         except ValueError:
             continue
         number_of_rules = int(input("\nВведите количество правил: "))
@@ -161,4 +172,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
